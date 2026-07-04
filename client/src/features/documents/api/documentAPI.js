5f185425c7ae441260392fc32eede6918cbff77d
TejasBhoite0707 <tejasbhoite3006@gmail.com>
2026-07-04 14:18:07 +0530

    Remaining Change Done

--- client/src/features/documents/api/documentAPI.js
+++ client/src/features/documents/api/documentAPI.js
@@ -55,5 +55,5 @@ export const deleteDocument = async (documentId) => {
 
 export const getDocumentUrl = (documentId) => {
 
-    return `${import.meta.env.VITE_API_URL}/documents/view/${documentId}`;
+    return `${process.env.VITE_API_URL}/documents/view/${documentId}`;
 }
